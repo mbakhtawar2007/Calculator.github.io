da389ec03399bdd04715db795206b006aa742365
MuhammadBakhtawar <m.bakhtawar.sce@gmail.com>
2024-03-14 16:31:31 +0500

    Calculator

---
+++ index.js
@@ -0,0 +1,12 @@
+const names = [];
+
+function Add() {
+  const a = parseInt(document.getElementById(`num1`).value);
+  const b = parseInt(document.getElementById(`num2`).value);
+}
+
+function Sub() {}
+
+function Mult() {}
+
+function Div() {}
